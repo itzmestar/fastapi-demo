2e15eca474ad539f6354a71a7e570f8256a12e00
Tarique Anwer <itzmetariq@gmail.com>
2024-06-26 22:06:46 +0530

    added router to main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from routers import (
-    router_account,
+    router_account, router_posts
 )
 
 app = FastAPI()
 app.include_router(router_account.router)
+app.include_router(router_posts.router)
 
 
 if __name__ == "__main__":
